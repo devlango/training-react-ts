fef667cd0decd366f3a459bb13194a7baeca02d8
Michal Podniesinski <m.podniesinski@azagroup.eu>
2018-08-20 16:27:07 +0200

    3. Rendering elements

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-interface IUser {
-    firstName: string,
-    lastName: string
-}
-
-function formatName(user: { firstName: string, lastName: string }): string {
-    return user.firstName + ' ' + user.lastName;
-}
-
-function getGreeting(user: IUser): JSX.Element {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Nobody to greet!</h1>;
-}
-
-const user: IUser = {
-    firstName: 'Grzegorz',
-    lastName: 'Prychowiak'
-};
-
-const element: JSX.Element = (
-    <div className='greeting'>
-        {getGreeting(user)}
-        <h2>Good to see you here.</h2>
-    </div>
-);
-
-// const elementJsx = (
-//     <h1 className="greeting">
-//         Hello, world!
-//     </h1>
-// );
-
-// const elementPure = React.createElement(
-//     'h1',
-//     {className: 'greeting'},
-//     'Hello, world!'
-// );
-
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-);
+function tick() {
+    const element: JSX.Element = (
+        <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  
+  // react elements are immutable
+  // react-dom updates only what is necessary
+  setInterval(tick, 1000);
