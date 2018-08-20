94eb077c0b1a4d41fdffd4a6c3510ad51f11143e
Michal Podniesinski <m.podniesinski@azagroup.eu>
2018-08-20 16:20:52 +0200

    2. Introduction JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,47 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+interface IUser {
+    firstName: string,
+    lastName: string
+}
+
+function formatName(user: { firstName: string, lastName: string }): string {
+    return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user: IUser): JSX.Element {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Nobody to greet!</h1>;
+}
+
+const user: IUser = {
+    firstName: 'Grzegorz',
+    lastName: 'Prychowiak'
+};
+
+const element: JSX.Element = (
+    <div className='greeting'>
+        {getGreeting(user)}
+        <h2>Good to see you here.</h2>
+    </div>
+);
+
+// const elementJsx = (
+//     <h1 className="greeting">
+//         Hello, world!
+//     </h1>
+// );
+
+// const elementPure = React.createElement(
+//     'h1',
+//     {className: 'greeting'},
+//     'Hello, world!'
+// );
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
